3aac56ffdc4b9a7e2d94b65a9843777dbcc1f5dd
james.fsantos1 <jamesf.santos@hotmail.com>
2026-04-28 16:31:14 -0300

    fix: Arrumando bugs

--- ByTech_API/ByTech_API/Controllers/AuthController.cs
+++ ByTech_API/ByTech_API/Controllers/AuthController.cs
@@ -22,24 +22,27 @@ namespace ByTech_API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginDto login)
         {
-            // 2. Valida no banco
-            var usuario = await _usuarioService.ObterPorEmail(login.Email);
+            var token = _authService.Autenticar(login);
 
-            // 3. Na hora de gerar o token e o retorno, IGNORE o login.Role 
-            // e use o dadosUsuario.TipoUsuario
-            var token = _authService.GerarToken(
-                usuario.Email,
-                usuario.TipoUsuario.Nome, 
-                usuario.Nome
-            );
+            
+            if (token == null)
+            {
+                return Unauthorized(new { mensagem = "E-mail ou senha inválidos" });
+            }
+
+            
+            var usuario = await _usuarioService.ObterPorEmail(login.Email);
 
             return Ok(new
             {
                 token = token,
                 usuarioEmail = usuario.Email,
                 usuarioNome = usuario.Nome,
-                role = usuario.TipoUsuario.Nome 
+                role = usuario.TipoUsuario.Nome
             });
+
+
+            
         }
     }
 }
